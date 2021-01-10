0fe50cbd8fd9009a0d51ed24da0456b0ef6ad81e
Demetris Konstantinides <demetris.konst@gmail.com>
2021-01-10 12:13:20 +0200

    Added support for EMD

--- EMD/utils/metrics.py
+++ EMD/utils/metrics.py
@@ -1,7 +1,11 @@
+from math import sqrt
 import numpy as np
 
 def manhattan(a, b):
     return np.linalg.norm(a-b, ord=1)
 
 def euclidean(a, b):
-    return np.linalg.norm(a-b, ord=2)
+    xa, ya = a
+    xb, yb = b
+    dist = sqrt((xa-xb)**2 + (ya-yb)**2)
+    return dist
